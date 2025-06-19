2898fb14b68076f67d32843c3043d5d30ec8b84b
xsantcastx <43703069+xsantcastx@users.noreply.github.com>
2025-06-19 14:53:05 +0200

    ajustes 1.0.4

--- js/carganoticias.js
+++ js/carganoticias.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("../data/noticias.json")
+    fetch("data/noticias.json")
         .then(response => response.json())
         .then(noticias => {
             const noticiasContainer = document.getElementById("contenedor-noticias");
